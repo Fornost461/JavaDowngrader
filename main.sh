6fc5382fb6a0d0cb3f02acde90073e0f6d51652c
Fornost461 <Fornost461@users.noreply.github.com>
2014-03-01 23:24:30 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#	This is free and unencumbered software released into the public domain.
+##	This is free and unencumbered software released into the public domain.
 #	Anyone is free to copy, modify, publish, use, compile, sell, or
 #	distribute this software, either in source code form or as a compiled
 #	binary, for any purpose, commercial or non-commercial, and by any
 #	means.
-#	Please refer to <http://unlicense.org/>.
+##	Please refer to <http://unlicense.org/>.
 
+
+# running this script will have the same effect as running main.scpt
 
 echo "Starting..."
 
