57f6e7e0e92009857de5cbced448bb1253c164c7
Fornost461 <Fornost461@users.noreply.github.com>
2014-03-01 23:22:14 +0100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#	This is free and unencumbered software released into the public domain.
+#	Anyone is free to copy, modify, publish, use, compile, sell, or
+#	distribute this software, either in source code form or as a compiled
+#	binary, for any purpose, commercial or non-commercial, and by any
+#	means.
+#	Please refer to <http://unlicense.org/>.
+
+
+echo "Starting..."
+
+#	Uninstall Java 7
+sudo rm -Rf /Library/Internet\\ Plug-Ins/JavaAppletPlugin.plugin
+
+#	Remove the Java 7 JDK if you installed it
+sudo rm -Rf /Library/Java/JavaVirtualMachines/jdk1.7*.jdk
+
+#	Re-enable Java 6
+sudo ln -sf /System/Library/Java/Support/Deploy.bundle/Contents/Resources/JavaPlugin2_NPAPI.plugin /Library/Internet\\ Plug-Ins/JavaAppletPlugin.plugin
+sudo ln -sf /System/Library/Frameworks/JavaVM.framework/Commands/javaws /usr/bin/javaws
+
+echo "Over! Java 6 should now work!"
